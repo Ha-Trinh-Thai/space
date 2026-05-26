831037b6881e445b3517f44f26382709f27d32ce
Ha Trinh <ha.trinh@yojee.com>
2026-05-26 17:51:05 +0700

    18

--- services/src/app.module.ts
+++ services/src/app.module.ts
@@ -1,23 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { WorkspaceModule } from './workspace/workspace.module';
-import { DocumentModule } from './document/document.module';
-import { CommentModule } from './comment/comment.module';
-import { CanvasModule } from './canvas/canvas.module';
-import { MindmapModule } from './mindmap/mindmap.module';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule,
-    AuthModule,
-    WorkspaceModule,
-    DocumentModule,
-    CommentModule,
-    CanvasModule,
-    MindmapModule,
-  ],
-})
+@Module({})
 export class AppModule {}
