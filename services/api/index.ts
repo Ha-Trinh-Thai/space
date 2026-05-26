d0c2eef28896951b6799b14ad9e5a9bbed65333b
Ha Trinh <ha.trinh@yojee.com>
2026-05-26 17:35:28 +0700

    14

--- services/api/index.ts
+++ services/api/index.ts
@@ -1,27 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
-import serverless from 'serverless-http';
-
-import { AppModule } from '../src/app.module';
-
-const expressApp = express();
-
-let cachedServer: any;
-
-async function bootstrap() {
-  if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-
-    await nestApp.init();
-
-    cachedServer = serverless(expressApp);
-  }
-
-  return cachedServer;
-}
-
-export default async function handler(req: any, res: any) {
-  const server = await bootstrap();
-  return server(req, res);
+export default function handler(req: any, res: any) {
+  return res.status(200).json({
+    ok: true,
+  });
 }
